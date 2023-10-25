bb062364a8a462945ecf388a30dec9b0ccd96595
sulochan <sulochankhadka25@gmail.com>
2023-10-25 10:22:02 +0530

    The app

---
+++ UserApp/src/screens/HomeScreen/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Dimensions} from 'react-native';
+
+import HomeMap from '../../components/HomeMap';
+import CovidMessage from '../../components/CovidMessage';
+import HomeSearch from '../../components/HomeSearch';
+
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <View style={{height: Dimensions.get('window').height - 400}}>
+        <HomeMap />
+      </View>
+
+      {/*  Bottom Comp*/}
+      <HomeSearch />
+    </View>
+  );
+};
+
+export default HomeScreen;
